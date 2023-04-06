275b953cc47ef8a84e6f5b16724038febf60e1f8
yinshiyi-Debian <yinzhengfan@163.com>
2023-04-06 23:58:43 +0800

    add golang env

--- function/function-common.sh
+++ function/function-common.sh
@@ -7,8 +7,13 @@ function proxyOff() {
 }
 
 function proxyOn() {
+    if [ ! -n  "$1" ];then
+        echo "Please input proxy URL"
+        return 0
+    fi
     export http_proxy=$1
     export https_proxy=$1
     export no_proxy="localhost,127.0.0.1,localhostaddress"
     curl google.com
+    return
 }
