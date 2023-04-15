aa330e2ea1076c4f55899f603a1fa2e980fdb6b2
yinshiyi-Debian <yinzhengfan@163.com>
2023-04-16 02:29:27 +0800

    add timestamp to date function

--- function/function-common.sh
+++ function/function-common.sh
@@ -17,3 +17,20 @@ function proxyOn() {
     curl google.com
     return
 }
+
+# attention: Additional outputt with timezone
+# timestamp to date
+function ttdate() {
+    echo $(date -d @$1 +"%Y-%m-%d %H:%M:%S")
+    echo $(date -d @$1 +"%Y-%m-%d %H:%M:%S %Z")
+}
+
+# attention: Timezone depends on your setting/mmachine
+# date to timestamp
+function datett() {
+    if [ ! -n "$1" ]; then
+        echo $(date +%s)
+    else
+        echo $(date -d "$1" +%s)
+    fi
+}
